aceea15804a9269a14eed0e7311dbca9601ab7e2
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-03 22:13:38 +0530

    Create n fibonacci num.py

---
+++ n fibonacci num.py
@@ -0,0 +1,28 @@
+"""
+Given a number N, print the first N Fibonacci numbers.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. The first line of each test case contains the integer N.
+
+Output:
+Print the first n fibonacci numbers with a space between each number. Print the answer for each test case in a new line.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1<= T <=100
+1<= N <=84
+
+Example:
+Input:
+2
+7
+5
+
+Output:
+1 1 2 3 5 8 13
+1 1 2 3 5
+
+Explanation: Some of the numbers of the Fibonacci numbers are 1, 1, 2, 3, 5, 8, 13 ..... (N stars from 1).
+"""
