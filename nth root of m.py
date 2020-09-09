7ec6d1a979b2b53bdf511ca785d427c10ca6d7b7
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-09 13:35:35 +0530

    Update nth root of m.py

--- nth root of m.py
+++ nth root of m.py
@@ -24,12 +24,16 @@ Output:
 import math
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    rt = m ** (1/n)
-    fl = math.floor(rt)
-    ce = math.ceil(rt)
-    if m == fl**n:
-        print(fl)
-    elif m == ce**n:
-        print(ce)
+    if n==0:
+        print(1)
     else:
-        print(-1)
+        rt = m ** (1/n)
+        fl = math.floor(rt)
+        ce = math.ceil(rt)
+        if m == fl**n:
+            print(fl)
+        elif m == ce**n:
+            print(ce)
+        else:
+            print(-1)
+
