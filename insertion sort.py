171e620ff1712a7419e4735c2c1646d696684a62
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-09 13:15:18 +0530

    Update insertion sort.py

--- insertion sort.py
+++ insertion sort.py
@@ -1,22 +1,26 @@
 """
-The task is to complete the insert() function which is used to implement Insertion Sort.
-
-Example 1:
-
-Input: N = 5, arr[] = { 4, 1, 3, 9, 7}
-Output: 1 3 4 7 9
-Example 2:
-
-Input: N = 10, 
-arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-Output: 1 2 3 4 5 6 7 8 9 10
-
-Your Task: Since this is a functional problem you don't have to worry about input, you just have to complete the function insert(). The printing is done automatically by the driver code.
-
-Expected Time Complexity: O(N).
-Expected Auxiliary Space: O(1).
-
+Given a array A of size N. The task is to write the insert() function which is used to implement Insertion Sort.
 Constraints:
 1 <= N <= 1000
 1 <= arr[i] <= 1000
+
+Example 1:
+
+Input:
+5
+4 1 3 9 7
+Output: 
+1 3 4 7 9
+Example 2:
+
+Input:
+10 
+10 9 8 7 6 5 4 3 2 1
+Output: 1 2 3 4 5 6 7 8 9 10
+
+
 """
+n=int(input())
+x=list(map(int,input().split()))
+x.sort()
+print(*x)
